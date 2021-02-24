585fae0b52ba8c281b8eda2767bdd9006ea983a2
P Nehitha <72698978+nehitha15107@users.noreply.github.com>
2021-02-24 18:50:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import {View,Text,Button,StyleSheet} from 'react-native';
+import AppHeader from './components/AppHeader';
+import HomeScreen from './screens/HomeScreen';
+import SummaryScreen from './screens/SummaryScreen';
+import {createAppContainer, createSwitchNavigator}from 'react-navigation';
+export default class App extends React.Component{
+
+  render(){
+    return(
+      <View>
+      <AppHeader/>
+      <AppContainer/>
+      </View>
+    )
+  }
+  
+
+}
+var AppNavigator = createSwitchNavigator({
+  HomeScreen:HomeScreen,
+  SummaryScreen:SummaryScreen,
+  
+})
+
+const AppContainer = createAppContainer(AppNavigator)
+
+
